1688f6ef9c30e003ac6fcefcd0ed023c2d197718
Sebastian <sebastian@iamsebastian.dev>
2021-06-28 22:48:53 +0200

    added folderstructure & basic webserver, created class

---
+++ public/application/app.mjs
@@ -0,0 +1,13 @@
+/** @format */
+
+/*
+
+    Create a new instance of Vay and provide it with the imported dictionary
+
+*/
+
+import de from './service/dictionary.de.mjs';
+
+import { Vay } from '../vay.js';
+
+console.log(Vay);
---
+++ readme.md
@@ -0,0 +1,5 @@
+## CLI
+
+--npm run dev to start development server
+--npm run start to run server
+--npm run build to minify the file
