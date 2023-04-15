cca9bf0a8ed0cc85ce52dcabdcba14914660e509
Sebastian <sebastian@iamsebastian.dev>
2023-04-16 00:31:44 +0200

    feat: create error enum

---
+++ src/lib/VayError.ts
@@ -0,0 +1,7 @@
+/** @format */
+
+export enum VayError {
+    NO_DICT = 'No dictionary found.',
+    NO_PHRASE = 'No phrase could be matched to token [{{phrase}}].',
+    MALFORMED_PHRASE = 'The token [{{token}}] is not a valid Phrase containing only numeric keys to evaluate for plural forms.',
+}
