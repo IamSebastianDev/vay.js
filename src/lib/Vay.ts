8d903733d8e61bbb80b5923dac936c3ab4d978bb
Sebastian <sebastian@iamsebastian.dev>
2023-04-22 11:18:27 +0200

    refactor: change private propery implementation

--- src/lib/Vay.ts
+++ src/lib/Vay.ts
@@ -5,27 +5,26 @@ import { getCount, interpolateString } from '../utils';
 import { VayError } from './VayError';
 
 export class Vay<T extends Dictionary<Record<string, Phrase>>> {
-    #targetAttribute!: string;
-    #targetElement!: Element;
-    #ignoreAttributes!: boolean;
-    #removeAttributesOnRender!: boolean;
-    #quiet!: boolean;
+    private _targetAttribute!: string;
+    private _targetElement!: Element;
+    private _ignoreAttributes!: boolean;
+    private _removeAttributesOnRender!: boolean;
+    private _quiet!: boolean;
 
-    #dicts!: T[];
+    private _dicts!: T[];
 
-    #currentLanguage: ISO639Code;
+    private _currentLanguage: ISO639Code;
 
     constructor(dicts: T[], init: VayConfig, initialLanguage?: ISO639Code) {
         Object.assign(
             this,
             Object.fromEntries(
                 Object.entries(init).map(([key, value]) => {
-                    return [`#${key}`, value];
+                    return [`_${key}`, value];
                 })
             )
         );
-
-        this.#dicts = dicts.map((dict) => {
+        this._dicts = dicts.map((dict) => {
             return {
                 ...dict,
                 // parse all locales to lowercase to ensure exact matching
@@ -33,17 +32,17 @@ export class Vay<T extends Dictionary<Record<string, Phrase>>> {
             };
         });
 
-        this.#currentLanguage = this.#inferInitialLanguage(initialLanguage);
+        this._currentLanguage = this._inferInitialLanguage(initialLanguage);
     }
 
-    #inferInitialLanguage(initialLanguageCode: ISO639Code | undefined): ISO639Code {
-        const dictionaryLanguageCodes = this.#dicts.map(({ locale }) => locale);
+    private _inferInitialLanguage(initialLanguageCode: ISO639Code | undefined): ISO639Code {
+        const dictionaryLanguageCodes = this._dicts.map(({ locale }) => locale);
 
         if (initialLanguageCode && dictionaryLanguageCodes.includes(initialLanguageCode)) {
             return initialLanguageCode;
         }
 
-        const [defaultLanguage, ...rest] = this.#getBrowserDefaultLanguages();
+        const [defaultLanguage, ...rest] = this._getBrowserDefaultLanguages();
 
         if (dictionaryLanguageCodes.includes(defaultLanguage)) {
             return defaultLanguage;
@@ -58,7 +57,7 @@ export class Vay<T extends Dictionary<Record<string, Phrase>>> {
         return dictionaryLanguageCodes[0];
     }
 
-    #getBrowserDefaultLanguages(): ISO639Code[] {
+    private _getBrowserDefaultLanguages(): ISO639Code[] {
         return [
             ...new Set(
                 window.navigator.languages.map((language) => {
@@ -68,23 +67,23 @@ export class Vay<T extends Dictionary<Record<string, Phrase>>> {
         ];
     }
 
-    #report(msg: VayError, interpolate: Record<string, string> = {}) {
-        if (this.#quiet) return;
+    private _report(msg: VayError, interpolate: Record<string, string> = {}) {
+        if (this._quiet) return;
 
         const interpolatedString = interpolateString(msg, interpolate);
         console.warn(`[Vay.js]: ${interpolatedString}`);
     }
 
     setLanguage(code: ISO639Code): void {
-        const dictionaryLanguageCodes = this.#dicts.map(({ locale }) => locale);
+        const dictionaryLanguageCodes = this._dicts.map(({ locale }) => locale);
 
         if (dictionaryLanguageCodes.includes(code)) {
-            this.#currentLanguage = code;
+            this._currentLanguage = code;
 
             window.dispatchEvent(
                 new CustomEvent('languageHasChanged', {
                     detail: {
-                        localeCode: this.#currentLanguage,
+                        localeCode: this._currentLanguage,
                     },
                 })
             );
@@ -92,15 +91,15 @@ export class Vay<T extends Dictionary<Record<string, Phrase>>> {
     }
 
     getLanguage(): ISO639Code {
-        return this.#currentLanguage;
+        return this._currentLanguage;
     }
 
     translate<K extends PropertyPath<T['phrases']>>(token: K, tData?: TranslationData, language?: ISO639Code) {
         const exitWithError = (error: VayError) => {
-            this.#report(error, { token });
+            this._report(error, { token });
             return token;
         };
-        const dict = this.#dicts.find(({ locale }) => locale === language || this.#currentLanguage);
+        const dict = this._dicts.find(({ locale }) => locale === language || this._currentLanguage);
 
         if (!dict) {
             return exitWithError(VayError.NO_DICT);
@@ -142,8 +141,8 @@ export class Vay<T extends Dictionary<Record<string, Phrase>>> {
         return token;
     }
 
-    #processToken(element: Element): void {
-        const token = element.getAttribute(this.#targetAttribute) as any;
+    private _processToken(element: Element): void {
+        const token = element.getAttribute(this._targetAttribute) as any;
         const translated = this.translate(token);
 
         // if no translation was found, return early to indicate
@@ -152,16 +151,16 @@ export class Vay<T extends Dictionary<Record<string, Phrase>>> {
             return;
         }
 
-        const method = element.hasAttribute(`${this.#targetAttribute}-html`) ? 'innerHTML' : 'textContent';
+        const method = element.hasAttribute(`${this._targetAttribute}-html`) ? 'innerHTML' : 'textContent';
         element[method] = translated;
     }
 
-    #processAttributes(element: Element): void {
-        if (this.#ignoreAttributes) return;
+    private _processAttributes(element: Element): void {
+        if (this._ignoreAttributes) return;
 
         const attributeSet = [...new Set(element.getAttributeNames())].filter(
             (attributeName) =>
-                attributeName.startsWith(this.#targetAttribute) && !(attributeName === `${this.#targetAttribute}-html`)
+                attributeName.startsWith(this._targetAttribute) && !(attributeName === `${this._targetAttribute}-html`)
         );
 
         attributeSet.forEach((attribute) => {
@@ -170,7 +169,7 @@ export class Vay<T extends Dictionary<Record<string, Phrase>>> {
             element.setAttribute(attributeName, this.translate(element.getAttribute(attribute) as any));
         });
 
-        if (this.#removeAttributesOnRender) {
+        if (this._removeAttributesOnRender) {
             attributeSet.forEach((attribute) => {
                 element.removeAttribute(attribute);
             });
@@ -180,10 +179,10 @@ export class Vay<T extends Dictionary<Record<string, Phrase>>> {
     render(): void {
         // Get all elements with a attribute matching the supplied targetAttribute
         // Iterate them to set the static translations
-        const markedElements = [...this.#targetElement.querySelectorAll(`[${this.#targetAttribute}]`)];
+        const markedElements = [...this._targetElement.querySelectorAll(`[${this._targetAttribute}]`)];
         markedElements.map((element) => {
-            this.#processToken(element);
-            this.#processAttributes(element);
+            this._processToken(element);
+            this._processAttributes(element);
         });
     }
 }
