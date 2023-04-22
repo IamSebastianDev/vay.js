2a975bec2fa1f91f92fe01d11c9f57ac9c68f1c4
Sebastian <sebastian@iamsebastian.dev>
2023-04-22 11:35:03 +0200

    fix: resolve issue with multiple interpolation parameters

--- src/utils/interpolateString.util.ts
+++ src/utils/interpolateString.util.ts
@@ -1,5 +1,4 @@
 /** @format */
 
-export const interpolateString = (string: string, data: Record<PropertyKey, string> = {}) => {
-    return string.replaceAll(/{{.*}}/gim, (r) => data[r.substring(2, r.length - 2)]);
-};
+export const interpolateString = (string: string, data: Record<PropertyKey, string> = {}) =>
+    string.replaceAll(/{{[^{]*}}/gim, (r) => data[r.substring(2, r.length - 2)]);
