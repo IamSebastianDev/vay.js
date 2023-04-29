9a22e1b755e04f4444d11e15e5775829c1900841
Sebastian <sebastian@iamsebastian.dev>
2023-04-29 20:22:42 +0200

    fix: set correct global name for browser package

--- .config/rollup.config.js
+++ .config/rollup.config.js
@@ -37,7 +37,7 @@ export default [
                 format: 'iife',
                 sourcemap: true,
                 plugins: [terser()],
-                name: 'straem',
+                name: 'Vay',
             },
         ],
     }),
