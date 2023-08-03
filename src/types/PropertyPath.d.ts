856ff3954ab4a1902384821bca2a1e99638ccb6e
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-08-03 19:21:10 +0200

    fix: change path parsing implementation restore nested property path parsing ability

--- src/types/PropertyPath.d.ts
+++ src/types/PropertyPath.d.ts
@@ -3,7 +3,7 @@
 import { Phrase } from './Phrase';
 
 export type PropertyPath<T, K extends keyof T = keyof T> = K extends string
-    ? T[k] extends Record<string, Record<number, string>>
+    ? T[any] extends Record<string, Record<number, string>>
         ? K
         : T[K] extends Record<string, Phrase>
         ? `${K}.${PropertyPath<T[K]>}`
