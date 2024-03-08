a3c160b856166ed8ec44e5d64e6df7f5f56b349f
Sebastian Heinz <sebsatian@iamsebastian.dev>
2024-03-08 18:08:30 +0100

    chore: add missing cdn bundle to package

--- packages/@vay/vay/.config/rollup.config.js
+++ packages/@vay/vay/.config/rollup.config.js
@@ -5,6 +5,7 @@ import esbuild from 'rollup-plugin-esbuild';
 import cleanup from 'rollup-plugin-cleanup';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 import pkg from '../package.json' assert { type: 'json' };
 
 const bundle = (config) => ({
@@ -26,6 +27,17 @@ export default [
                 file: pkg.module,
                 format: 'es',
                 sourcemap: true,
+            },
+            /**
+             * The IIFE bundle is used for delivery via unpgk CDN and as the browser package file. It will work in any
+             * browser, even if it doesn't support ESM Modules, imports or exports.
+             */
+            {
+                file: pkg.unpkg,
+                format: 'iife',
+                sourcemap: true,
+                plugins: [terser()],
+                name: 'Vay',
             },
         ],
     }),
