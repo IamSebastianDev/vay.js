1578b7610e98d8382ec8de5960d3350523953df0
Sebastian Heinz <sebsatian@iamsebastian.dev>
2024-03-17 22:48:55 +0100

    refactor: add default language to config

--- packages/@vayjs/vay/src/lib/define-config.ts
+++ packages/@vayjs/vay/src/lib/define-config.ts
@@ -1,1 +1,29 @@
 /** @format */
+
+import { VayConfig } from '../types/vay-config';
+
+const defaultConfig: VayConfig = {
+    targetAttribute: 'vay',
+    ignoreAttributes: false,
+    removeAttributesOnRender: false,
+    quiet: true,
+    defaultLocale: 'en',
+};
+
+/**
+ * @description
+ * Method used to create a valid Vay configuration object. Mostly used to enable autocomplete in modern IDEs.
+ *
+ * @param { Partial<VayConfig> } [props] - optional object containing properties to override the default
+ * configuration properties of the config object.
+ * @returns { VayConfig } a valid Vay configuration object.
+ */
+
+export const defineConfig = (props: Partial<VayConfig> = {}): VayConfig => {
+    return {
+        // default configuration properties
+        ...defaultConfig,
+        // merge user supplied configuration properties
+        ...props,
+    };
+};
