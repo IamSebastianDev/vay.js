a14b729f8efaecdcc3248ae7e459039551c03f62
Sebastian Heinz <sebsatian@iamsebastian.dev>
2024-03-18 19:37:18 +0100

    chore: create skeleton for publishing script

---
+++ packages/publish/publish.ts
@@ -0,0 +1,7 @@
+/** @format */
+
+import { bold } from 'kolorist';
+import pkg from './package.json';
+
+console.clear();
+console.log(`Publishing ${bold(`Vay.js`)} using Publisher@${pkg.version}`);
