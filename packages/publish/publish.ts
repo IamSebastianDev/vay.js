c60553a0caaf5f8a1705ebdb41a7b7996031edc3
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-04-09 19:49:02 +0200

    chore: remove publish package

--- packages/publish/publish.ts
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-import { bold } from 'kolorist';
-import pkg from './package.json';
-
-console.clear();
-console.log(`Publishing ${bold(`Vay.js`)} using Publisher@${pkg.version}`);
